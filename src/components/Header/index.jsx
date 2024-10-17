95cd3180f80a4a7a689eb19dbb5febff247a7ef2
Aritra Dey <adey01027@gmail.com>
2024-10-17 13:44:06 +0530

    fixed responsiveness

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 function Header() {
   return (
     <div className="mt-10 flex flex-col gap-5 items-center justify-center text-gray-800 roboto-serif-heading no-select">
-      <h1 className="text-6xl sm:text-7xl lg:text-8xl font-bold text-center leading-tight font-baskerville ">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl font-bold text-center leading-tight font-baskerville">
         STARTUP
       </h1>
-      <h1 className="text-6xl sm:text-7xl lg:text-8xl font-bold text-center leading-tight font-baskerville">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl font-bold text-center leading-tight font-baskerville">
         INTERNSHIP
       </h1>
-      <h1 className="text-6xl sm:text-7xl lg:text-8xl font-bold text-center leading-tight font-baskerville">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl font-bold text-center leading-tight font-baskerville">
         PORTAL
       </h1>
       <p className="text-xl sm:text-2xl lg:text-3xl text-center font-medium text-gray-600 playfair-display-heading">
